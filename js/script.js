239b4293c35c50679953c06b7710156228ebe19e
Maksym Polinovskyi <mmykesser@gmail.com>
2024-02-23 16:44:17 +0100

    Homework #6 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
+// Task 1
+
+const userName = prompt('Enter your name:');
+const userAge= prompt('Enter your age:');
+const userLastName = prompt('Enter your last name:');
+
+console.log(userName+' '+userLastName+' '+ userAge + ' years' );
+
+// Task 2
+
+let info = '12345';
+info = info.split('', 5);
+console.log(info);
+
+
+
+
 
