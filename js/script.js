ee5b949c7612b062caa4bb18a4d2b3c4c332033d
Maksym Polinovskyi <mmykesser@gmail.com>
2024-02-23 18:03:00 +0100

    Homework #7 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 'use strict';
 
+// Task #1
+const name = 'Jonh';
+let admin;
+admin=name;
+
+alert(admin);
+
+// Task #2
+const namePlanet = 'Earth';
+console.log('Name planet: '+namePlanet);
+const nameUser = 'Rayan';
+console.log('User name: '+nameUser);
+
+// Task #3
+
+let ex1 = 10 +'10' ;
+let ex2 =10;
+
+console.log(ex1+' Type of: ' + typeof ex1);
+console.log(ex2+' Type of: '+ typeof ex2);
+
 
