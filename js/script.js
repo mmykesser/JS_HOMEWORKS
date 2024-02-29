28626222c79cc8c99d0338e509a623b6e70829d2
Maksym Polinovskyi <mmykesser@gmail.com>
2024-02-29 23:35:36 +0100

    Homework #9 completed

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ const type = prompt('Specify the unit of measurement where F is Fahrenheit and C
 
 let result;
 
- if ( typeof temp === 'number' && type === 'F') {
-    result = ('It will be ')+(temp - 32) * 5 / 9 + (' degrees Fahrenheit');
-} else  if (type === 'C'){
-    result = ('It will be ')+((temp * 9 / 5) + 32) + (' degrees Celsius');
+if (isNaN(temp)){
+    result = 'Error, please enter a number';
+} else if ( type.toUpperCase() === 'F') {
+    result = 'It will be '+(temp - 32) * 5 / 9 + ' degrees Celsius';
+} else  if (type.toUpperCase() === 'C'){
+    result = 'It will be '+((temp * 9 / 5) + 32) + ' degrees Fahrenheit';
 } else
-    result = ('Error, check the entered data and try again ');
+    result = 'Error, select the correct temperature unit';
 
 alert(result);
 
