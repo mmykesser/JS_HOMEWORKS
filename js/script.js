430435b0760c61d7e9d1e603ef909b108b8a9487
Maksym Polinovskyi <mmykesser@gmail.com>
2024-02-29 18:41:01 +0100

    Homework #8

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
+const temp = +prompt('Please specify the temperature :');
+const type = prompt('Specify the unit of measurement where F is Fahrenheit and C is Celsius :');
+
+let result;
+
+ if ( typeof temp === 'number' && type === 'F') {
+    result = ('It will be ')+(temp - 32) * 5 / 9 + (' degrees Fahrenheit');
+} else  if (type === 'C'){
+    result = ('It will be ')+((temp * 9 / 5) + 32) + (' degrees Celsius');
+} else
+    result = ('Error, check the entered data and try again ');
+
+alert(result);
 
