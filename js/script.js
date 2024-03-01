61e4c5dcec44daac3ac78e9ffe68ae48c3d4229e
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-01 16:11:24 +0100

    Homework #10 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 'use strict';
 
+const number1 = +prompt('Please enter the first number:');
+if (!isNaN(number1)) {
+    const number2 = +prompt('Please enter the second number:');
+    if (!isNaN(number2)) {
+        console.log(`Addition: ${number1}+${number2} = ${number1+number2}`);
+        console.log(`Subtraction: ${number1}-${number2} = ${number1-number2}`);
+        console.log(`Multiplication: ${number1}*${number2} = ${number1*number2}`);
+        console.log(`Division: ${number1}/${number2} = ${number1/number2}`);
 
+    } else {
+        alert('Error,please enter second number');
+    }
+} else {
+        alert('Error,please enter the first number');
+}
