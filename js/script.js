747a5a9c0d012599c206a4716c1a3928bee303ce
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-05 17:41:49 +0100

    Homework #16 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 'use strict';
 
+const firstNumber = Number(prompt('Please enter the first number'));
+
+if (!isNaN(firstNumber)) {
+    const secondNumber = Number(prompt('Please enter the second number'));
+    if (!isNaN(secondNumber)) {
+        const thirdNumber = Number(prompt('Please enter the third number'));
+        if (!isNaN(thirdNumber)) {
+            const sum = (firstNumber + secondNumber + thirdNumber) / 3;
+            console.log('Arithmetic mean is '+ sum);
+        } else {
+            console.log('Error, please enter only numbers');
+        }
+    } else {
+        console.log('Error, please enter only numbers');
+    }
+} else {
+    console.log('Error, please enter only numbers');
+}
+
+
+
 
