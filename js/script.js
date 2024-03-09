6321b238a8debf0dabd9afea02586cd0e5dc31bb
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-10 00:34:04 +0100

    Homework #17 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
 'use strict';
 
+const year = prompt('Please enter your year of birth');
+const city = prompt('What city do you live in?');
+const sport = prompt('What is your favorite sport? \n Boxing, Football, Athletics ? ');
+
+let age;
+let country;
+let champion;
+
+if (year === null) {
+    age = ('It\'s a pity you didn\'t want to enter your age');
+} else {
+    age = 2024 - year;
+}
+
+if (city == null) {
+    country = ('It\'s a pity you didn\'t want to share your city of residence');
+} else if (city === 'London') {
+    country = 'You live in the capital of England';
+} else if (city === 'Kyiv') {
+    country = 'You live in the capital of Ukraine';
+} else if (city === 'Washington') {
+    country = 'You live in the capital of the United States';
+} else {
+    country = 'You live in a ' + city;
+}
+
+if (sport === null) {
+    champion = 'It\'s a pity that you didn\'t want to specify your favorite sport';
+} else {
+    switch (sport) {
+        case 'Boxing':
+            champion = 'Cool! Do you want to be Wladimir Klitschko?';
+            break;
+        case 'Football':
+            champion = 'Cool! Do you want to be Cristiano Ronaldo?';
+            break;
+        case 'Athletics':
+            champion = 'Cool! Do you want to be Usain Bolt?';
+            break;
+        default:
+            champion = 'Become a champion';
+    }
+}
+
+alert('Your age is ' + age + ' years old \n'
+    + country + '\n' + champion);
+
+
+
 
