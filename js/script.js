2cf1ff6baf93a3184f49ad9a633f02add193488f
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-10 02:20:53 +0100

    Homework #18 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr === '':
+        console.log('Empty String');
+        break;
+    case isNaN(numOrStr):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
