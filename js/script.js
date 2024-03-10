f402c0f30de48ba1a96bddfa3e7db5f50b1ca990
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-10 03:03:21 +0100

    Homework #19 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
+const year = prompt('Введіть число');
 
+switch (true){
+    case isNaN(+year):
+        alert('Введіть будь ласка число');
+        break;
+    case year <= 1:
+        alert('1 рік');
+        break;
+    case year <= 4:
+        alert(year + ' роки');
+        break;
+    case year > 4:
+        alert(year + ' років');
+        break;
+    default:
+        alert( "Немає таких значень" );
+}
