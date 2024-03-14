2baa94377e10fa6255da0fee6f82b1b7024b814c
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-14 14:10:01 +0100

    Homework #25 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
+const num = Number(prompt('Please enter the number'));
+let result = '';
+if (num < 1) {
+    result = 'NaN';
+} else if (num === 1) {
+    result = 'Minimum divisor of a number ' + num;
+} else {
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            result = 'Minimum divisor of a number ' + num + '. This is the ' + i;
+            break;
+        } else {
+            result = 'Minimum divisor of a number ' + num + '. This is the ' + i;
+        }
+    }
+}
+console.log(result);
+
+
 
