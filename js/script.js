54d447a4c55ee555c648b93ed54e5900e1a479fc
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-14 04:04:57 +0100

    Homework #24 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
+// Task 1
+const arr1 = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i <= arr1.length; i++) {
+    sum = sum + i;
+}
+console.log('Sum of elements = ' + sum);
+
+// Task 2
+const arr2 = [1, 2, 3, 4, 5];
+sum = 0;
+for (let i = 0; i <= arr2.length; i++) {
+    sum += Math.pow(i, 2);
+}
+console.log('Sum of squares of array element = ' + sum);
+
+
 
