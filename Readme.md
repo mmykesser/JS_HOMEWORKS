d76610a3791e0377ea719b329551a0e3b730c6dc
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-15 23:47:12 +0100

    Homework #27 completed

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
-### Homework #1
+### Homework #27
+
+Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>  
+  
+Наприклад:  
+  
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';  
+
+const key = generateKey(16, characters);  
+console.log(key); // eg599gb60q926j8i  
--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const generateKey = function (length, characters) {
+    let key = '';
+    for (let i = 1; i <= length; i++) {
+        const mix = Math.floor(Math.random() * characters.length);
+        key += characters[mix];
+    }
+    return key;
+}
+
+console.log(generateKey(characters.length, characters));
+
 
