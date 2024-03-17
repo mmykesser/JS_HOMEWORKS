38e0ecbbe0c66de77e4084ce2b30eb913fd877a2
Maksym Polinovskyi <mmykesser@gmail.com>
2024-03-17 16:02:35 +0100

    Homework #28 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
+const num = prompt('Please enter the number');
+const deg = prompt('To what degree should the number be raised?');
 
+const raisingDegree = function (num, deg = 1) {
+    if (isNaN(num) || isNaN(deg)) {
+        return 'Error, please enter a number!';
+    } else {
+        return num ** deg;
+    }
+}
+
+const result = raisingDegree(num, deg);
+alert(result);
